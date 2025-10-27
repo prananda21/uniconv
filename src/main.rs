aea8b1c38689501b7c69a620e43522858e88ce94
pranandayoga <pranandayoga21@gmail.com>
2025-10-27 15:58:10 +0800

    feat: add logic for calculating some value to all units

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::{Parser, Subcommand};
+
+mod conv;
+
+use conv::{Degree, Length, LengthConverter, TemperatureConverter};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Temperature Converter");
+    println!("========================");
+
+    // example 1: convert 25 C to all units
+    let temp_c = TemperatureConverter::new(25.0, Degree::Celsius);
+    let res = temp_c.convert_to_all();
+    println!("25 C convert to: {}", res);
+
+    // example 2: conver 100 cm to all units
+    let length_cm = LengthConverter::new(10.0, Length::Centimeter);
+    let res = length_cm.convert_to_all();
+    println!("100 cm convert to: {}", res);
 }
